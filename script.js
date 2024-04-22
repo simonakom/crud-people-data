5483fe461ad167aeff2cf1c606c5a6cc47f0bcaa
simonakomarovska <simonak399@gmail.com>
2024-04-22 23:01:18 +0200

    fixed responsive

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-//local storage
-console.log(localStorage.getItem("people")); //patikrinti ar egzistuoja masyvas (people) ir kokia to reiksme(jei neegzituoja-null reiksme)
+// ------------------------------LOCAL STORAGE --------------------------// 
+console.log(localStorage.getItem("people")); //check if array (people) exists and what its value (if not exists-null)
 
-//patikrinti ar kazkada buvo uzeita i si websaita, jie ne, yra sukuriamas irasas:
+//to check if the website has ever been accessed, if not, record is created:
 if (!localStorage.getItem("people")){
-    //jei tokio "key" people nera, laukelis buvo neuzpildytas, tada priskirti pradine reiksme siame local storage
-    //Tuscio masyvo issaugoti negalima.Tik teksines strnig reiksmes. Tad reikia prideti kabutes ('[]') arba JSON
+
+    // if no such "key" people exists, the field was unfilled, then assign the initial value in this local storage
+    // The empty array cannot be saved. Only string values. So you need to add quotes ('[]') or JSON
     localStorage.setItem("people", JSON.stringify([]));
 } 
-//Jeigu nusetiname zmogui reiksme, kita karta turetume gauti ta reiksme (is localstorage) i people masyva (kai yra apsilankymas, turime gauti visu zmomiu irasus is localstorage):
-//irasus gauname string formatu - JASON.parse keicia string i objekta
+
+// If we set a value for a person, the next time we should get that value (from localstorage) into the people array (when there is a visit, we have to get all people records from localstorage):
+// recordings are recived in string format - JASON.parse changes string into objekta
 const people = JSON.parse (localStorage.getItem("people")) 
  
-//Kai yra atnaujinamas pats masyvas- reikia atnaujiti localstorage esancius duomenys ----> ziureti prie create, delete,update
+//When the array is being updated - need to update the localstorage data ----> look under create, delete, update
 
-//kad atvaizduoti:
+//to display:
 generateTableContent (people)
 
 // const people = []; //An array to store information about people. is hidden as there is local storage
@@ -25,8 +27,7 @@ const allowedNationalities = ['US', 'CANADA', 'UK', 'AUSTRALIA', 'FRANCE', 'GERM
                     if(!localStorage.getItem('currentNumeration'))
                     localStorage.setItem("currentNumeration", "1");*/
 
-
-//selecting HTML elements 
+// -----------------------------HTML ELEMENTS -----------------------------// 
 const buttonElement = document.querySelector ("#add-button");
 const deleteElement = document.querySelector ("#delete-button");
 const findElement = document.querySelector ("#find-id");
@@ -47,8 +48,8 @@ resetFiltersButton = document.getElementById('resetFilters');
 
 modalElement = document.getElementById('productInfo')
 
-//FILTER: if Arrey is same for each input
-
+// -----------------------------FILTER-------------------------------// 
+//if Arrey is same for each input
 filterNameElement.onkeyup = filter; 
 filterLastNameElement.onkeyup = filter; 
 filterAgeElement.onkeyup = filter; 
@@ -73,7 +74,8 @@ function refreshPage() {
 location.reload();
 }
 
-//CREATE: When "Add" is pressed: 
+// -----------------------------CREATE-------------------------------// 
+//When "Add" is pressed
 buttonElement.addEventListener("click", ()=>{
     //When the "Add" button is clicked, a new person object is created from the form inputs.
     const person = {};
@@ -82,7 +84,6 @@ buttonElement.addEventListener("click", ()=>{
     person.age = +document.getElementById(`ageInput`).value;
     person.nationality = document.getElementById("nationalityInput").value.toUpperCase() 
     person.image = document.getElementById("imageInput").value;
-
     // console.log (person);
 
     // Validations:
@@ -144,20 +145,20 @@ person.number = people.length + 1;
 // If all checks pass, the person object is added to people array, and the table (generateTableContent) is updated.
 people.push(person);
 
-    //Pakeisti alert teksta
     addResult.style.display = 'block';
     addResult.innerText = 'New Person has been successfully added!';
-    //kad pakeisti spalva
     addResult.style.backgroundColor = '#1a995750';
+    addResult.style.padding = '10px';
 
 generateTableContent (people); //calls the generateTableContent function, passing the updated people array as an argument. The purpose of this function is to generate HTML content for displaying the list of people in a table. 
 formElement.reset(); //resets the form (formElement) after adding a person. It clears the input fields in the form.
          
-//Kai yra atnaujinamas pats masyvas- reikia atnaujiti localstorage esancius duomenys 
+//When the array itself is updated - the localstorage data must be updated 
 localStorage.setItem("people", JSON.stringify(people));
 }); 
 
-//READ: A function to generate HTML content for displaying the list of people in a table.
+// -----------------------------READ-------------------------------// 
+// displaying the list of people in a table
 function generateTableContent (people){
     let dynamicHTML = ``; 
     for(let person of people){
@@ -168,14 +169,14 @@ function generateTableContent (people){
         <td>${person.lastName}</td>
         <td>${person.age}</td>
         <td>${person.nationality}</td>   
-        <td onclick="showModal(${person.number})"><span id="photoLink_${person.number}" onclick="toggleLinkVisibility('${person.number}', '${person.image}')">Photo of ${person.firstName} ${person.lastName} </span></td>
+        <td onclick="showModal(${person.number})"><span class="photoLink" id="photoLink_${person.number}" onclick="toggleLinkVisibility('${person.number}', '${person.image}')">Photo of ${person.firstName} ${person.lastName} </span></td>
         </tr>`;
     }
     const tbody = document.querySelector(`table tbody`)
     tbody.innerHTML = dynamicHTML;
 }
 
-//MODAL
+// ------------------------------MODAL-------------------------------// 
 const showModal = (num) => { 
     let foundIndexDelete = people.findIndex((person) => person.number == num);
     const person = people[foundIndexDelete];
@@ -187,7 +188,7 @@ const showModal = (num) => {
         <div class="row">
             <span class="fw-bold bio">${person.firstName} ${person.lastName} - ${person.age} years, ${person.nationality} </span>
         </div>
-        <button id="close-button" type="button" class="btn btn-primary btn_hover butn my-3 py-2 px-5" onclick="modalElement.close()">Close</button>
+        <button id="close-button" type="button" class="btn btn-primary butn my-3 py-2 px-5" onclick="modalElement.close()">Close</button>
     </div> </div>`
     modalElement.innerHTML = dynamicHTML
     document.querySelector("#modalBody").onclick = (event) => {
@@ -200,15 +201,15 @@ modalElement.onclick = () => {
     modalElement.close();
 }
 
-//DELETE: When "delete" is pressed: 
+//-------------------------DELETE------------------------------// 
 deleteElement.addEventListener("click", ()=>{
     const removeElementImput = document.querySelector ("#numberInput"); //representing the number of the person to be deleted.
     let num = +removeElementImput.value;//It retrieves the numerical value from the input and stores it in the variable num. Alos makes it string.
-    removeElementImput.value = ''; //reiksmiu nusinulinimas
+    removeElementImput.value = ''; 
     deleteResult.style.display = "none";
 
-    // findindex grazina indeksa pagal elelemnto reiksme. Jei toks nebuvo rastas, grazina -1.
-    let foundIndexDelete = people.findIndex((person) => person.number == num) //The findIndex method is used to find the index of the person in the people array whose number matches the input value (num).If no matching person is found, foundIndexDelete is set to -1.
+    // findindex returns index according to the elemnt's value. If none is found, returns  -1.
+    let foundIndexDelete = people.findIndex((person) => person.number == num) //The findIndex method is used to find the index of the person in the people array whose number matches the input value (num).
 
     //validation: checks are performed on the input value.
     if (!num || isNaN(num)) {
@@ -229,10 +230,8 @@ deleteElement.addEventListener("click", ()=>{
         deleteResult.style.display = 'none';
     }
 
-    //Pakeisti alert teksta
     deleteResult.style.display = 'block';
     deleteResult.innerText = 'Selected Person has been successfully deleted!';
-    //kad pakeisti spalva
     deleteResult.style.backgroundColor = '#1a995750';
 
                         // (disabled as currentnumeration)*people.splice(foundIndexDelete,1);//If the validation checks pass, it uses splice to remove one element at the found index from the people array.
@@ -245,10 +244,11 @@ deleteElement.addEventListener("click", ()=>{
     });
     generateTableContent(people);//It then calls the generateTableContent function to update the table display with the modified people array.
 
-//Kai yra atnaujinamas pats masyvas- reikia atnaujiti localstorage esancius duomenys 
+//When the array itself is updated - the localstorage data must be updated 
 localStorage.setItem("people", JSON.stringify(people));
 }); 
 
+//-------------------------FIND------------------------------// 
 //declares a variable outside of any function, making it accessible globally. This variable will be used to store the index of the person with the specified number when the "Find" button is clicked.
 let foundIndexUpdate;
 
@@ -285,19 +285,17 @@ foundIndexUpdate = people.findIndex((person) => person.number == updateNumber); 
     document.getElementById("updateImageInput").value = foundPerson.image;
 
 
-    updateForm.style.display = 'block';//show form
-    findElement.style.display = 'none';//hide find button
-    updateElement.style.display = 'block';//show update button
-    updateResult.style.display = "none";//hide alert notes
+    updateForm.style.display = 'block';
+    findElement.style.display = 'none';
+    updateElement.style.display = 'block';
+    updateResult.style.display = "none";
 
-    //Pakeisti alert teksta
     updateResult.style.display = 'block';
     updateResult.innerText = 'Person has been found! Now you can update his data :)';
-    //kad pakeisti spalva
     updateResult.style.backgroundColor = '#1a995750';
 }); 
 
-//When "update" is pressed: 
+// ------------------------UPDATE------------------------------// 
 updateElement.addEventListener("click", () => {
     const updatedPerson = {};//An empty object is created to represent the updated person.
     //The properties of this object are populated with values from the update form inputs.
@@ -339,7 +337,7 @@ updateElement.addEventListener("click", () => {
     }
 
     if (!allowedNationalities.includes(updatedPerson.nationality)) {
-        updateResult.innerText = "Invalid nationality. Please enter a valid nationality: US, Canada, UK Australia, France, Germany, Japan";
+        updateResult.innerText = "Invalid nationality. Please enter a valid nationality: US, Canada, UK, Australia, France, Germany, Japan";
         updateResult.style.backgroundColor = '#cf7a847f';
         updateResult.style.display = 'block';
         return;
@@ -348,12 +346,12 @@ updateElement.addEventListener("click", () => {
     }
 
     // Validations: Duplicate person check is performed based on contact details.
-    const updatedFirstName = document.getElementById("updateFirstNameInput").value;
-    const updatedLastName = document.getElementById("updateLastNameInput").value;
-    
     const duplicatePerson = people.find((existingPerson) =>
-        existingPerson.firstName.toLowerCase() === updatedFirstName.toLowerCase() &&
-        existingPerson.lastName.toLowerCase() === updatedLastName.toLowerCase()
+    existingPerson.firstName.toLowerCase() === updatedPerson.firstName.toLowerCase() &&
+    existingPerson.lastName.toLowerCase() === updatedPerson.lastName.toLowerCase() &&
+    existingPerson.age === updatedPerson.age &&
+    existingPerson.nationality.toLowerCase() === updatedPerson.nationality.toLowerCase() &&
+    existingPerson.image.toLowerCase() === updatedPerson.image.toLowerCase()
     );
     
     if (duplicatePerson) {
@@ -368,10 +366,8 @@ updateElement.addEventListener("click", () => {
     // Update the person in the people array at the index foundIndexUpdate.
     people[foundIndexUpdate] = updatedPerson;
 
-    //Pakeisti alert teksta
     updateResult.style.display = 'block';
     updateResult.innerText = 'Person has been successfully updated!';
-    //kad pakeisti spalva
     updateResult.style.backgroundColor = '#1a995750';
 
     // Reset the form  (instead writing manually, use function)
@@ -382,11 +378,12 @@ updateElement.addEventListener("click", () => {
     // document.getElementById("updateAgeInput").value = "";
     // document.getElementById("updateNationalityInput").value = "";
 
-    updateForm.style.display = 'none'; //hide the update form
-    findElement.style.display = 'block'; //show find button
-    updateElement.style.display = 'none'; //hide update button
+    updateForm.style.display = 'none'; 
+    findElement.style.display = 'block'; 
+    updateElement.style.display = 'none'; 
 });
 
+// ------------------------CLEAR INPUTS------------------------------// 
 function nullifyInputValues (){
     document.querySelector("#numberInputUpdate").value = "";
     document.getElementById("updateFirstNameInput").value = "";
@@ -398,6 +395,6 @@ function nullifyInputValues (){
         // Update the table with the modified data.
         generateTableContent(people);
 
-        //Kai yra atnaujinamas pats masyvas- reikia atnaujiti localstorage esancius duomenys 
+        //When the array itself is updated - the localstorage data must be updated 
         localStorage.setItem("people", JSON.stringify(people));
 }
